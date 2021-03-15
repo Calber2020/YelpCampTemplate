b8c34973a9b10750cb289c311f792f26d238aed0
Chase <calber@students.solano.edu>
2021-03-14 19:52:20 -0700

    adding pages

--- base/app.js
+++ base/app.js
@@ -24,11 +24,16 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req, res) => {
     res.render('home')
 })
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'});
-    await camp.save();
-    res.send(camp)
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index')
 })
+
+app.get('/campgrounds/:id', async(req, res) => {
+    res.render('camgrounds/show')
+})
+
 
 app.listen(3001, () => {
     console.log('Serving on port 3001')
